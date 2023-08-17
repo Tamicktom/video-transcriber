052f9c0ec9559c3c61e8b0ba4f40569c235f8b33
Tamicktom <rickpokemon38@gmail.com>
2023-08-16 23:18:04 -0300

    saving

--- src/form.ts
+++ src/form.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 //* Local imports
 import { startLoading, stopLoading, loadingMessage } from "./loading";
 import { loadVideo, getVideoId } from "./youtube-api";
+import { transcribeAudio } from "./transcribe";
 
 const form = document.querySelector<HTMLFormElement>("#form")!;
 
@@ -20,7 +21,11 @@ form.addEventListener("submit", async (e) => {
     await loadVideo(url);
 
     loadingMessage("Baixando o video...");
-    await axios.get("http://localhost:3333/audio?v=" + getVideoId(url));
+    // await axios.get("http://localhost:3333/audio?v=" + getVideoId(url));
+
+    const data = await transcribeAudio(getVideoId(url));
+
+    console.log(data);
   } catch (error) {
     console.error("[SUBMIT ERROR]", error);
   } finally {
---
+++ src/transcribe.ts
@@ -0,0 +1,49 @@
+import { pipeline } from "@xenova/transformers";
+import { loadingMessage } from "./loading";
+
+// let data = null;
+import data from "./data.json";
+
+export type Chunk = {
+  timestamp: [number, number];
+  text: string;
+};
+
+export type Transcription = {
+  text: string;
+  chunks: Chunk[];
+};
+
+export async function transcribeAudio(videoId: string) {
+  const options = {
+    chunk_lenght_s: 30,
+    stride_length_s: 5,
+    language: "portuguese",
+    task: "transcribe",
+    return_timestamps: true,
+  };
+
+  try {
+    console.time();
+    loadingMessage("Transcribing audio...");
+    console.log("[START_TRANSCRIPTION]");
+
+    // const transcriber = await pipeline(
+    //   "automatic-speech-recognition",
+    //   "Xenova/whisper-small"
+    // );
+
+    console.log("[TRANSCRIBING_AUDIO]", videoId);
+    // data = await transcriber(`../vv${videoId}.mp3`, options);
+  } catch (error) {
+    console.error("[ERROR_TRANSCRIBE]", error);
+    //@ts-ignore
+    throw new Error(error);
+  } finally {
+    console.timeEnd();
+    loadingMessage("Transcription complete!");
+    console.log("[END_TRANSCRIPTION]");
+  }
+
+  return data as Transcription;
+}
